83b105072215d71827435ae08eeaeaea378ad830
James White <james.offwhite@gmail.com>
2019-03-18 10:36:27 +0000

    improved role creation

--- app/assets/javascripts/new_role.js
+++ app/assets/javascripts/new_role.js
@@ -3,13 +3,13 @@ var ready = function(){
   prep_role_type_lookup();
 
   $('.submit-row').hide();
-  $('.focus_onload').first().focus();
+  $('.page-content input[type=text], .page-content input[type=date], .page-content textarea').first().focus();
 };
 
 function prep_profile_lookup(){
-  $('.profile_lookup').keyup(profile_lookup);
-  $('.profile_lookup').focus(profile_lookup);
-  $('.profile_lookup').blur(profile_name_blur);
+  $('input.profile_lookup').keyup(profile_lookup);
+  $('input.profile_lookup').focus(profile_lookup);
+  $('.profile_lookup').blur(show_save_button);
   $('body').on('click', '.profile_lookup_list li', set_selected_profile);
 }
 
@@ -17,20 +17,16 @@ function set_selected_profile(){
   var id = $(this).data('id')
   var name = $(this).find('.profile-name').html()
   $('input.profile_lookup_id').val(id)
-  $('.profile_lookup').html(name)
+  $('input.profile_lookup').val(name)
 
   $('.profile_lookup_list ').slideUp();
-}
-
-function profile_name_blur(){
-  $('input.profile_name').val($(this).html());
-  show_save_button();
+  $('input.role_type_lookup').focus();
 }
 
 function profile_lookup(){
   $('input.profile_lookup_id').val('')
   show_save_button();
-  var string = $(this).html();
+  var string = $(this).val();
   if(string.length < 3){return}
 
   $.ajax({
@@ -42,8 +38,8 @@ function profile_lookup(){
 };
 
 function prep_role_type_lookup(){
-  $('.role_type_lookup').keyup(role_type_lookup);
-  $('.role_type_lookup').focus(role_type_lookup);
+  $('input.role_type_lookup').keyup(role_type_lookup);
+  $('input.role_type_lookup').focus(role_type_lookup);
 
   $('body').on('click', '.role_type_lookup_list li', set_selected_role_type);
 }
@@ -52,7 +48,7 @@ function set_selected_role_type(){
   var id = $(this).data('id')
   var name = $(this).find('.role-type-name').html()
   $('input.role_type_lookup_id').val(id)
-  $('.role_type_lookup').html(name)
+  $('.role_type_lookup').val(name)
 
   $('.role_type_lookup_list ').slideUp();
   show_save_button()
@@ -61,7 +57,7 @@ function set_selected_role_type(){
 function role_type_lookup(){
   $('input.role_type_lookup_id').val('')
   show_save_button();
-  var string = $(this).html();
+  var string = $(this).val();
   if(string.length < 3){return}
 
   $.ajax({
@@ -75,12 +71,12 @@ function role_type_lookup(){
 function show_save_button(){
   if(
     $('input.role_type_lookup_id').val() == '' ||
-    $('input.profile_name').val().length < 3
+    $('input.profile_lookup').val().length < 3
   ){
     $('.submit-row').slideUp();
     return
   }
-  console.log($('input.profile_name').val().length)
+  console.log($('input.profile_lookup').val().length)
   $('.submit-row').slideDown();
 }
 
--- app/controllers/profiles/lookup.rb
+++ app/controllers/profiles/lookup.rb
@@ -2,14 +2,19 @@ module Profiles
   class Lookup < ::ControllerAction
     def call
       expose(profiles, '@profiles')
+      expose(query_str, '@query_str')
     end
 
     private
 
     def profiles
       @profiles ||=
-        Profile.matches(params[:search])
+        Profile.matches(query_str)
                .limit(20)
+    end
+
+    def query_str
+      @query_str ||= params[:search]
     end
   end
 end
