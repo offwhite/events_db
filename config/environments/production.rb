ca457c9f0ae83af2da7be53d03588939a36c7c35
James White <james.offwhite@gmail.com>
2019-03-20 17:29:19 +0000

    integrate with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "eventsdb_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'http://crewdb.co.uk', port: 80 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -89,4 +87,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'crewdb.co.uk',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
