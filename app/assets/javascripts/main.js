ef04a161845b6c126c4852b3855e535217f28410
James White <james.offwhite@gmail.com>
2019-03-28 15:55:36 +0000

    add an its me button to the event add crew view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,6 +2,7 @@ var ready = function(){
   prepConfirmations();
   prepMobileMenus();
   prepCollapsibles();
+  prepMyProfile();
 };
 
 var prepConfirmations = function(){
@@ -26,5 +27,16 @@ var prepCollapsibles = function() {
     $(this).parent().find('.panel-body').slideToggle();
   });
 }
+
+var prepMyProfile = function() {
+  $('.my-profile').click(function(e){
+    id = $(this).data('id');
+    name = $(this).data('name');
+    $('.profile_lookup_id').val(id);
+    $('.profile_lookup').val(name);
+    e.preventDefault();
+    return false;
+  });
+};
 
 $(document).on('turbolinks:load', ready);
