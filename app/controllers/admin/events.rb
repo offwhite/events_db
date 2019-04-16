1f687eb46b5a6707e9130d5f9e638bf9440b1a2a
James White <james.offwhite@gmail.com>
2019-04-16 13:41:04 +0100

    allow events to be filtered in admin

--- app/controllers/admin/events.rb
+++ app/controllers/admin/events.rb
@@ -2,12 +2,33 @@ module Admin
   class Events < ::AdminControllerAction
     def call
       expose(events, '@events')
+      expose(type, '@type')
     end
 
     private
 
     def events
-      @events ||= Event.order(id: :desc).page(params[:page])
+      events_by_type || all_events
+    end
+
+    def all_events
+      @all_events ||= Event.order(id: :desc).page(params[:page])
+    end
+
+    def events_by_type
+      return if type.nil?
+      @event_by_type ||=
+        Event.where(event_type: type.id)
+             .order(id: :desc)
+             .page(params[:page])
+    end
+
+    def type
+      @type ||= EventType.find(type_id) if type_id.present?
+    end
+
+    def type_id
+      @type_id ||= params[:type_id]
     end
   end
 end
